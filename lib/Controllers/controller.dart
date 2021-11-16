c114ecf3ed2ba595346500574c3b5d98b3fd44ea
Julian Castro <julianan.castro@urosario.edu.co>
2021-11-16 12:38:20 -0500

    My profile update

--- lib/Controllers/controller.dart
+++ lib/Controllers/controller.dart
@@ -2,7 +2,13 @@
 import 'package:get/get.dart';
 
 class Controller extends GetxController {
-  var _name = 'Pepito Perez'.obs;
+  var _name = 'Carlos Sebastian'.obs;
+  var _surname = 'Martinez Vidal'.obs;
+  var _cedula = 1006108401.obs;
+  var _email = 'sebastianmartinezvidal@gmail.com'.obs;
+  var _cellphone = 3016264162.obs;
+  var _date = DateTime(2000, 05, 26).obs;
+  var _gender= true.obs;
   var _imagePath = 'assets/images/Profile_Image.png'.obs;
   var _listProducts = [
     ['Papa pastusa', 'assets/images/papa.png'],
@@ -25,6 +31,12 @@ class Controller extends GetxController {
   List<List> get listProducts => [..._listProducts];
   List<String> get notifications => [..._notifications];
   bool get isLog => _isLog.value;
+  String get surname => _surname.value;
+  int get cedula => _cedula.value;
+  String get email => _email.value;
+  int get cellphone => _cellphone.value;
+  DateTime get date => _date.value;
+  bool get gender => _gender.value;
 
   String? validator(String value) {
     if (value.isEmpty) {
--- lib/Screens/Pages/Profile/ProfileMenu/my_profile.dart
+++ lib/Screens/Pages/Profile/ProfileMenu/my_profile.dart
@@ -1,132 +1,91 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:trueque/Controllers/controller.dart';
+import 'package:flutter/material.dart';
+import 'package:trueque/Screens/Pages/Profile/Components/auxi.dart';
 
-class MyProfile extends StatelessWidget {
-  MyProfile({Key? key}) : super(key: key);
+class MyProfile extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
 
-  final nameController = TextEditingController();
-  final pathController = TextEditingController();
+class _EditProfilePageState extends State<MyProfile> {
   final Controller controller = Get.put(Controller());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const Text(
-          'Trueque',
-          style: TextStyle(fontSize: 35, color: Colors.black),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-            onPressed: () {
-              Get.back();
-            },
-            icon: const Icon(
-              Icons.arrow_back_outlined,
-              color: Colors.black,
-            )),
-      ),
-      body: GetX<Controller>(
-        init: controller,
-        builder: (Controller widCtrl) {
-          return Center(
-            child: Column(
-              children: [
-                TextField(
-                  textAlign: TextAlign.center,
-                  controller: nameController,
-                  onSubmitted: (value) {
-                    if (nameController.text != '') {
-                      widCtrl.usernameChanged(nameController.text);
-                      nameController.clear();
-                    }
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Nombre',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    if (nameController.text != '') {
-                      widCtrl.usernameChanged(nameController.text);
-                      nameController.clear();
-                    }
-                  },
-                  icon: Icon(Icons.thumb_up_alt_rounded),
-                ),
-                Text('Nombre: ${widCtrl.name}'),
-                TextField(
-                  textAlign: TextAlign.center,
-                  controller: pathController,
-                  onSubmitted: (value) {
-                    if (pathController.text != '') {
-                      widCtrl.imgProfileChanged(pathController.text);
-                      nameController.clear();
-                    }
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Path: ',
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    if (pathController.text != '') {
-                      widCtrl.imgProfileChanged(pathController.text);
-                      pathController.clear();
-                    }
-                  },
-                  icon: Icon(Icons.thumb_up_alt_rounded),
-                ),
-                Text('Path: ${widCtrl.imagePath}'),
-              ],
+    String fecha =
+        "${controller.date.year.toString()}/${controller.date.month.toString().padLeft(2, '0')}/${controller.date.day.toString().padLeft(2, '0')}";
+    //Size size = MediaQuery.of(context).size;
+    var imageasset = new AssetImage(controller.imagePath);
+    var image = new Image(image: imageasset, fit: BoxFit.cover);
+    return GetX<Controller>(
+      init: controller,
+      builder: (Controller widCtrl) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            title: const Text(
+              'Trueque',
+              style: TextStyle(fontSize: 35, color: Colors.black),
             ),
-          );
-        },
-      ),
+            centerTitle: true,
+            leading: IconButton(
+                onPressed: () {
+                  Get.back();
+                },
+                icon: const Icon(
+                  Icons.arrow_back_outlined,
+                  color: Colors.black,
+                )),
+          ),
+          body: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            physics: BouncingScrollPhysics(),
+            children: [
+              const SizedBox(height: 15),
+              ProfileWidget(imagePath: image, onClicked: () async {}),
+              const SizedBox(height: 30),
+              TextFieldWidget(
+                label: 'Name',
+                text: widCtrl.name,
+                onChanged: (name) {},
+              ),
+              const SizedBox(height: 20),
+              TextFieldWidget(
+                label: 'Surname',
+                text: widCtrl.surname,
+                onChanged: (surname) {},
+              ),
+              const SizedBox(height: 20),
+              TextFieldWidget(
+                label: 'ID number',
+                text: '${widCtrl.cedula}',
+                // ignore: non_constant_identifier_names
+                onChanged: (cedula) {},
+              ),
+              const SizedBox(height: 20),
+              TextFieldWidget(
+                label: 'E-mail',
+                text: widCtrl.email,
+                onChanged: (email) {},
+              ),
+              const SizedBox(height: 20),
+              TextFieldWidget(
+                label: 'Mobile phone',
+                text: '${widCtrl.cellphone}',
+                onChanged: (cellphone) {},
+              ),
+              const SizedBox(height: 20),
+              TextFieldWidget(
+                label: 'Date of birth',
+                text: fecha,
+                onChanged: (date) {},
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
-  /* 
-  Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const Text(
-          'Trueque',
-          style: TextStyle(fontSize: 35, color: Colors.black),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-            onPressed: () {
-              Get.back();
-            },
-            icon: const Icon(
-              Icons.arrow_back_outlined,
-              color: Colors.black,
-            )),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-                textAlign: TextAlign.center,
-                controller: nameController,
-                onChanged: (nameText) => _search(controller, nameText),
-                decoration: InputDecoration(
-                  hintText: 'Nombre',
-                  border: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(),
-                )),
-            Obx(() => Text('Nombre: ${controller.name}'))
-          ],
-        ),
-      ),
-    )
-  */
 }
